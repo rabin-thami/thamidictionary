7d72d953527f2b4f3f29bc9b3be4824897c01320
rabin-thami <rabinthami36@gmail.com>
2025-09-24 23:41:43 +0545

    feat: signup, reset function added

--- src/app/auth/forget-password/page.tsx
+++ src/app/auth/forget-password/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState, useTransition } from "react";
